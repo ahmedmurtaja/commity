53d6a792555a06bf8d904fad00451c275dcf692d
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-04-18 23:03:44 +0200

    fix(cli): fix y/n Relates #5        Closes #5

--- index.js
+++ index.js
@@ -45,41 +45,6 @@ const choices = [
   { name: "other: Doesn't fit any of the suggested types?", value: 'other' },
 ];
 
-// generate chooices for inquirer
-const inquirerChoices = choices.map((c) => {
-  return {
-    name: c.name,
-    value: c.value,
-  };
-});
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'type',
-    message: 'Select the type of commit you want to make:',
-    choices: inquirerChoices,
-  },
-  {
-    type: 'input',
-    name: 'message',
-    message: 'Enter the commit message:',
-  },
-]).then((answers) => {
-  if (answers.type === 'other') {
-    const type = prompt('Enter the type of commit: ');
-    message = `${type}: ${answers.message}`;
-  } else {
-    message = `${answers.type}: ${answers.message}`;
-  }
-  exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
-    if (error) {
-      log('Something went wrong. Please try again.'.red);
-      return;
-    }
-    log(`Commit successful`.green.bold);
-  });
-});
-
 
 
 const sleep = (milliseconds) => {
@@ -142,94 +107,102 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// let selectedIndex = 0;
+let selectedIndex = 0;
 
-// function displayChoices() {
-//   console.clear();
-//   log('Select the type of commit you want to make:'.green);
-//   log('Use arrow keys to navigate. Press enter to select.');
-//   choices.forEach((c, idx) => {
-//     if (idx === selectedIndex) {
-//       console.log(
-//         colors.bgYellow.bold.green(`
-//       > ${idx + 1}. ${c.name}  `)
-//       );
-//     }
-//   });
-// }
+function displayChoices() {
+  console.clear();
+  log('Select the type of commit you want to make:'.inverse);
+  log('Use arrow keys to navigate. Press enter to select.'.inverse);
+  choices.forEach((c, idx) => {
+    if (idx === selectedIndex) {
+      console.log(
+        colors.bgYellow.bold.green(`
+      > ${idx + 1}. ${c.name}  `)
+      );
+    }
+  });
+}
 
-// displayChoices();
+displayChoices();
 
-// const handleUnStagedFiles = (files) => {
-//   exec(`git add .`, (error, stdout, stderr) => {
-//     if (error) {
-//       return;
-//     }
-//   });
-//   exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
-//     if (error) {
-//       log('Something went wrong. Please try again.'.red);
-//       return;
-//     }
-//     log(`Commit successful`.green.bold);
-//   });
-// };
-// const commit = () => {
-//   exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
-//     if (error) {
-//       if (error.code === 1) {
-//         log('You have un staged files.'.red);
-//         log(execSync('git status --porcelain', { encoding: 'utf-8' }));
-//         log(` Would you like to add them? (y/n)`.red);
-
-//         const answer = readlineSync.question('y/n: ', {
-//           limit: ['y', 'n'],
-//           limitMessage: 'Please enter y or n',
-//           hideEchoBack: false,
-//         });
-//         if (answer === 'y') {
-//           handleUnStagedFiles();
-//         } else {
-//           log('Exiting...'.red);
-//           log('Please stage your files and try again.'.red);
-//           log('use: git add <file-name>'.red);
-//           process.exit(0);
-//         }
-//       }
-//     }
-//     else log(`Commit successful`.green.bold);
-
-//     rl.close();
-//   });
-
-// }
-// rl.input.on('keypress', (_, key) => {
-//   if (key.name === 'up') {
-//     selectedIndex = Math.max(selectedIndex - 1, 0);
-//     displayChoices();
-//   } else if (key.name === 'down') {
-//     selectedIndex = Math.min(selectedIndex + 1, choices.length - 1);
-//     displayChoices();
-//   } else if (key.name === 'return') {
-//     const choice = choices[selectedIndex];
-//     console.log(`You selected: ${choice.name}`);
-//     message += choice.value;
-//     const scope = prompt('Enter the scope of the work you done: ');
-//     message += `(${scope}): `;
-//     const description = prompt('Enter a description of the work you done: ');
-//     message += `${description}`;
-//     const issueNumber = prompt('Enter the issue number: ');
-//     message += ` Relates #${issueNumber}`;
-//     if (readlineSync.keyInYN('Do you want to close the issue?')) {
-//       message += `
-//        Closes #${issueNumber}`;
-//     }
-//     log(`\n Your commit message is:  `.yellow + `${message}`.green);
-
-//     if (readlineSync.keyInYN('Do you want to commit?')) {
-//       commit();
-//     }
+const handleUnStagedFiles = (files) => {
+  exec(`git add .`, (error, stdout, stderr) => {
+    if (error) {
+      return;
+    }
+  });
+  exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
+    if (error) {
+      log('Something went wrong. Please try again.'.red);
+      log("Here's the error message:".red);
+      log(error.message);
+      return;
+    }
+    log(`Commit successful`.green.bold);
+  });
+};
+const commit = () => {
+  exec(`git commit -m "${message}"`, (error, stdout, stderr) => {
+    if (error) {
+      if (error.code === 1) {
+        log('You have un staged files.'.red);
+        // handle colors of un staged files
+        let color = execSync('git status --porcelain', { encoding: 'utf-8' });
+        color = color.split(' ');
+        color = color.map((c) => c.replace('M', 'M'.white.bgYellow.bold));
 
 
-//   }
-// });
+        log(color.join(' '));
+        log(` Would you like to add them? (y/n)`.red);
+
+        const answer = readlineSync.question('y/n: ', {
+          limit: ['y', 'n'],
+          limitMessage: 'Please enter y or n',
+          hideEchoBack: false,
+        });
+        if (answer === 'y') {
+          handleUnStagedFiles();
+        } else {
+          log('Exiting...'.red);
+          log('Please stage your files and try again.'.red);
+          log('use: git add <file-name>'.red);
+          process.exit(0);
+        }
+      }
+    }
+    else log(`Commit successful`.green.bold);
+
+    rl.close();
+  });
+
+}
+rl.input.on('keypress', (_, key) => {
+  if (key.name === 'up') {
+    selectedIndex = Math.max(selectedIndex - 1, 0);
+    displayChoices();
+  } else if (key.name === 'down') {
+    selectedIndex = Math.min(selectedIndex + 1, choices.length - 1);
+    displayChoices();
+  } else if (key.name === 'return') {
+    const choice = choices[selectedIndex];
+    console.log(`You selected: ${choice.name}`);
+    message += choice.value;
+    const scope = prompt('Enter the scope of the work you done: ');
+    message += `(${scope}): `;
+    const description = prompt('Enter a description of the work you done: ');
+    message += `${description}`;
+    const issueNumber = prompt('Enter the issue number: ');
+    message += ` Relates #${issueNumber}`;
+    if (readlineSync.keyInYN('Do you want to close the issue?')) {
+      message += `
+       Closes #${issueNumber}`;
+    }
+    log(`\n Your commit message is:  `.yellow + `${message}`.green);
+
+    if (readlineSync.keyInYN('Do you want to commit?')) {
+      commit();
+    }
+
+
+  }
+});
